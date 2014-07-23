b391f86f7a0dbd0ca906e8d36b8a33f2f11abb5d
Elliot Murphy <elliot.murphy@gmail.com>
2014-07-23 09:53:41 -0400

    Fix configure error.

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,11 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+if defined?(Bundler)
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+end
 
 module Treebook
   class Application < Rails::Application
